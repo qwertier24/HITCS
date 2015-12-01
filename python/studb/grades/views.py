3e9f506cde4ed2397ec85337ecd36e1c86389005
Yukun Ma <gmayukun@gmail.com>
2015-12-01 11:32:34 +0800

    studb v6

--- python/studb/grades/views.py
+++ python/studb/grades/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from models import Student
+from django.db.models import Max,Sum,Avg
 import csv
+
+subjects = ['tot','chi','mat','eng','phy','che']
+subjects_ch = ["总","语文","数学","英语","物理","化学"]
 
 def index(request):
     return render(request, 'grades/index.html')
@@ -22,27 +26,15 @@ def query_stu(request):
         return render(request, 'grades/error.html', {"str":"学生信息输入错误，请重试"});
 
 def query_all(request):
-    order = request.GET.get('order', 0)
-    order = int(order)
+    order = int(request.GET.get('order', 0))
     low = request.GET.get('low', 0)
     high = request.GET.get('high', 10000)
     if low == '':
         low = 0
     if high == '':
         high = 10000
-    if order == 0:
-        stus = Student.objects.order_by('-tot').filter(tot__gte=low).filter(tot__lte=high)
-    elif order == 1:
-        stus = Student.objects.order_by('-chi').filter(chi__gte=low).filter(chi__lte=high)
-    elif order == 2:
-        stus = Student.objects.order_by('-mat').filter(mat__gte=low).filter(mat__lte=high)
-    elif order == 3:
-        stus = Student.objects.order_by('-eng').filter(eng__gte=low).filter(eng__lte=high)
-    elif order == 4:
-        stus = Student.objects.order_by('-phy').filter(phy__gte=low).filter(phy__lte=high)
-    elif order == 5:
-        stus = Student.objects.order_by('-che').filter(che__gte=low).filter(che__lte=high)
-    return render(request, 'grades/query.html', {"stus":stus})
+    stus = Student.objects.order_by('-'+subjects[order]).filter(**{subjects[order]+'__gte':low}).filter(**{subjects[order]+'__lte':high})
+    return render(request, 'grades/query.html', {"stus":stus, "subid":order})
 
 def modify0(request):
     if request.method=='POST':
@@ -61,19 +53,13 @@ def modify1(request):
         cmd = request.REQUEST.get('cmd')
         num = int(request.POST.get('num'))
         if cmd == 'edit':
-            name =request.POST.get('name')
-            chi = int(request.POST.get('chi'))
-            mat = int(request.POST.get('mat'))
-            eng = int(request.POST.get('eng'))
-            phy = int(request.POST.get('phy'))
-            che = int(request.POST.get('che'))
-            Student.objects.filter(num=num).update(name = name,
-                                                chi = chi,
-                                                mat = mat,
-                                                eng = eng,
-                                                phy = phy,
-                                                che = che,
-                                                tot = chi + mat + eng + phy + che)
+            paras = dict()
+            paras['name'] = request.POST.get('name')
+            paras['tot'] = 0
+            for subject in subjects[1:]:
+                para[subject] = request.POST.get(subject)
+                paras['tot'] += para[subject]
+            Student.objects.filter(num=num).update(**paras)
             return render(request, 'grades/query.html', {"stus":Student.objects.filter(num=num)})
         else:
             Student.objects.filter(num=num).delete()
@@ -95,13 +81,12 @@ def upload(request):
                         continue
                     if len(Student.objects.filter(num=int(words[0]))) == 0:
                         Student.objects.create(num=int(words[0]))
-                    Student.objects.filter(num=int(words[0])).update(name=words[1],
-                                                                     chi=int(words[2]),
-                                                                     mat=int(words[3]),
-                                                                     eng=int(words[4]),
-                                                                     phy=int(words[5]),
-                                                                     che=int(words[6]),
-                                                                     tot=sum([int(i) for i in words[2:7]]))
+                    paras = dict()
+                    for i in xrange(1,6):
+                        paras[subjects[i]] = int(words[i+1])
+                        paras['tot'] += paras[subjects[i]]
+                    paras['name']=words[1]
+                    Student.objects.filter(num=int(words[0])).update(**paras)
                 return render(request, 'grades/notify.html', {"str":"导入csv文件成功"})
         except:
             return render(request, 'grades/error.html', {'str':'您没有上传文件或文件格式错误，具体文件格式详见帮助'})
@@ -125,43 +110,27 @@ def help(request):
 def about(request):
     return render(request, 'grades/about.html')
 
-sub = ["总","语文","数学","英语","物理","化学"]
 def stat(request):
     order = int(request.GET.get('order',0))
     if order == '':
         order = 0
-        cnt_A=0
-        cnt_B=0
-        cnt_C=0
-        cnt_D=0
+    cnt_A = 0
+    cnt_B = 0
+    cnt_C = 0
+    cnt_D = 0
+    max_score = 0
     if order == 0:
-        cnt_A = len(Student.objects.filter(tot__gte=65*9))
-        cnt_B = len(Student.objects.filter(tot__gte=65*8).filter(tot__lt=65*9))
-        cnt_C = len(Student.objects.filter(tot__gte=65*6).filter(tot__lt=65*8))
-        cnt_D = len(Student.objects.filter(tot__lt=65*6))
-    elif order == 1:
-        cnt_A = len(Student.objects.filter(chi__gte=10*9))
-        cnt_B = len(Student.objects.filter(chi__gte=10*8).filter(chi__lt=10*9))
-        cnt_C = len(Student.objects.filter(chi__gte=10*6).filter(chi__lt=10*8))
-        cnt_D = len(Student.objects.filter(chi__lt=10*6))
-    elif order == 2:
-        cnt_A = len(Student.objects.filter(mat__gte=10*9))
-        cnt_B = len(Student.objects.filter(mat__gte=10*8).filter(mat__lt=10*9))
-        cnt_C = len(Student.objects.filter(mat__gte=10*6).filter(mat__lt=10*8))
-        cnt_D = len(Student.objects.filter(mat__lt=10*6))
-    elif order == 3:
-        cnt_A = len(Student.objects.filter(eng__gte=10*9))
-        cnt_B = len(Student.objects.filter(eng__gte=10*8).filter(eng__lt=10*9))
-        cnt_C = len(Student.objects.filter(eng__gte=10*6).filter(eng__lt=10*8))
-        cnt_D = len(Student.objects.filter(eng__lt=10*6))
-    elif order == 4:
-        cnt_A = len(Student.objects.filter(phy__gte=10*9))
-        cnt_B = len(Student.objects.filter(phy__gte=10*8).filter(phy__lt=10*9))
-        cnt_C = len(Student.objects.filter(phy__gte=10*6).filter(phy__lt=10*8))
-        cnt_D = len(Student.objects.filter(phy__lt=10*6))
-    elif order == 5:
-        cnt_A = len(Student.objects.filter(che__gte=10*9))
-        cnt_B = len(Student.objects.filter(che__gte=10*8).filter(che__lt=10*9))
-        cnt_C = len(Student.objects.filter(che__gte=10*6).filter(che__lt=10*8))
-        cnt_D = len(Student.objects.filter(che__lt=10*6))
-    return render(request, 'grades/stat.html', {'cnt_A':cnt_A, 'cnt_B':cnt_B, 'cnt_C':cnt_C, 'cnt_D':cnt_D, 'sub':sub[order]})
+        max_score = 650
+    elif order <= 3:
+        max_score = 150
+    else:
+        max_score = 100
+    cnt_A = Student.objects.filter(**{subjects[order]+'__gte':max_score/10*9}).count()
+    cnt_B = Student.objects.filter(**{subjects[order]+'__gte':max_score/10*8}).filter(**{subjects[order]+'__lt':max_score/10*9}).count()
+    cnt_C = Student.objects.filter(**{subjects[order]+'__gte':max_score/10*6}).filter(**{subjects[order]+'__lt':max_score/10*8}).count()
+    cnt_D = Student.objects.filter(**{subjects[order]+'__lt':max_score/10*6}).count()
+    top = Student.objects.aggregate(Max(subjects[order]))[subjects[order]+'__max']
+    avg = Student.objects.aggregate(Avg(subjects[order]))[subjects[order]+'__avg']
+    stun = Student.objects.all().count()
+    mid = Student.objects.all().order_by(subjects[order]).values(subjects[order])[stun/2][subjects[order]]
+    return render(request, 'grades/stat.html', {'cnt_A':cnt_A, 'cnt_B':cnt_B, 'cnt_C':cnt_C, 'cnt_D':cnt_D, 'sub':subjects_ch[order], 'top':top, 'avg':avg, 'mid':mid, 'stun':stun})
